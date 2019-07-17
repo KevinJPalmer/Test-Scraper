9db28f4d61bafccc11ee211140986f92031e969f
Kevin Palmer <kpalmer4194@gmail.com>
2019-07-17 16:09:50 -0400

    updated url parser to correct mongoose error

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //dependencies
 var express = require("express");
+var mongoose = require("mongoose");
 var expressHandlebars = require("express-handlebars");
 var bodyParser = require("body-parser");
 //Set up port
@@ -28,6 +29,18 @@ app.use(bodyParser.urlencoded({
 //pass through router
 app.use(router);
 
+//
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+//connect mongoose to the db
+mongoose.connect(db, { useNewUrlParser: true }, function(error) {
+  if (error) {
+    console.log(error);
+  }
+  else {
+    console.log("mongoose is connected");
+  }
+});
 //listen to port
 app.listen(PORT, function() {
   console.log("listening on port:" + PORT);
